92aa2e4584cfc3ca5c8d3883f4fc539b53ac72e1
Junkai Yang <cyfloel0516@hotmail.com>
2017-04-21 20:57:20 -0400

    add community information to network class

--- jy_detection.py
+++ jy_detection.py
@@ -1,112 +1,112 @@
 import csv
 import networkx as nx
-import heapq
+import heapq, os
 from collections import defaultdict
 from network_stats import NetworkStats
 
+class JYCommunityDetection(object):
+    def __init__(self, local_threshold=0.3, node_threshold=0.7, jaccard_threshold=0.2):
+        self.local_threshold = local_threshold
+        self.node_threshold = node_threshold
+        self.jaccard_threshold = jaccard_threshold
+    
+    def _find_full_edge(self, network):
+        full_edges = []
+        for n1 in network.nodes():
+            for n2 in network.nodes():
+                # this edge has been checked
+                if sorted([n1, n2]) in full_edges:
+                    continue
+                # this edge is not a bi-direct edge
+                if not network.has_edge(n1, n2) or not network.has_edge(n2, n1):
+                    continue
+                
+                # add this edge to the full_edges array
+                full_edges.append(sorted([n1, n2]))
+        return full_edges
+    
+    def accuracy(self, communities, memberships, community_truth):
+        pass
 
-# return tuples
-def find_triangles(network):
-    # 1. find the bi-direction edge first
-    bidirect_edges = []
-    for n1 in network.nodes():
-        for n2 in network.nodes():
-            # this edge has been checked
-            if sorted([n1, n2]) in bidirect_edges:
-                continue
-            # this edge is not a bi-direct edge
-            if not network.has_edge(n1, n2) or not network.has_edge(n2, n1):
+    def detect(self, network):
+        full_edges = self._find_full_edge(network)
+        communities = []
+        community_hash = defaultdict(set)
+        index = 1
+        # 1. assume the nodes in a bi-edges are belong to the same community
+        for edge in full_edges:
+            n1, n2 = edge
+            local = set([n1, n2])
+            local_validated = set(local)
+            # the two nodes have been checked and assigned to the community
+            if n1 in community_hash and n2 in community_hash:
                 continue
             
-            # add this edge to the bidirect_edges array
-            bidirect_edges.append(sorted([n1, n2]))
-    return bidirect_edges
-    # print(bidirect_edges)
-    # 2. find all triangles
+            neighbors = set(list(network.successors(n1))  + list(network.predecessors(n1)) + list(network.successors(n2))  + list(network.predecessors(n2)) )
+            neighbor_scores = []
+            while True:
+                new_neighbors = neighbors
+                print(neighbors)
+                candidates = []
+                for neighbor in neighbors:
+                    # calculate the score of the neighbor node
+                    connected = 0
+                    for local_node in local:
+                        if network.has_edge(neighbor, local_node):
+                            connected += 1
+                    score = float(connected) / len(local)
+                    # heapq.heappush(neighbor_scores, (-score, neighbors))
+                    if score > self.local_threshold or float(connected) / len(network.successors(neighbor)) > self.node_threshold:
+                        candidates.append(neighbor)
 
-def find_community(network, bi_edges):
-    communities = []
-    community_hash = defaultdict(list)
-    index = 1
-    # 1. assume the nodes in a bi-edges are belong to the same community
-    for edge in bi_edges:
-        n1, n2 = edge
-        local = set([n1, n2])
-        local_validated = set(local)
-        # the two nodes have been checked and assigned to the community
-        if n1 in community_hash and n2 in community_hash:
-            continue
-        
-        neighbors = set(list(network.successors(n1))  + list(network.predecessors(n1)) + list(network.successors(n2))  + list(network.predecessors(n2)) )
-        neighbor_scores = []
-        while True:
-            new_neighbors = neighbors
-            print(neighbors)
-            candidates = []
-            for neighbor in neighbors:
-                # calculate the score of the neighbor node
-                connected = 0
-                for local_node in local:
-                    if network.has_edge(neighbor, local_node):
-                        connected += 1
-                score = float(connected) / len(local)
-                # heapq.heappush(neighbor_scores, (-score, neighbors))
-                if score > 0.3 or float(connected) / len(network.successors(neighbor)) > 0.7:
-                    candidates.append(neighbor)
+                for candidate in candidates:
+                    # remove un-related candidate
+                    inside_connection = 0
+                    outside_connection = len(set(network.successors(candidate) + network.predecessors(candidate) + list(local)))
+                    for node in local:
+                        if network.has_edge(node, candidate) or network.has_edge(candidate, node):
+                            inside_connection += 1
+                    # print(candidate)
+                    # print(float(inside_connection) / outside_connection)
+                    if float(inside_connection) / outside_connection < 0:
+                        continue
+                    local.add(candidate)
+                    # neighbors.discard(neighbor
+                    new_neighbors = new_neighbors.union(list(network.successors(candidate))  + list(network.predecessors(candidate)))
 
-            for candidate in candidates:
-                # remove un-related candidate
-                inside_connection = 0
-                outside_connection = len(set(network.successors(candidate) + network.predecessors(candidate) + list(local)))
-                for node in local:
-                    if network.has_edge(node, candidate) or network.has_edge(candidate, node):
-                        inside_connection += 1
-                # print(candidate)
-                # print(float(inside_connection) / outside_connection)
-                if float(inside_connection) / outside_connection < 0:
-                    continue
-                local.add(candidate)
-                # neighbors.discard(neighbor
-                new_neighbors = new_neighbors.union(list(network.successors(candidate))  + list(network.predecessors(candidate)))
+                new_neighbors  = new_neighbors - local
 
-            new_neighbors  = new_neighbors - local
-
-            # print(new_neighbors)
-            if new_neighbors == neighbors:
-                break
-            else:
-                neighbors = new_neighbors
-                for node in local:
-                    # final filter to check if the node is really belongs to the community
-                    node_neighbors = list(network.successors(node))  + list(network.predecessors(node))
-                    local_neighbors = [n for n in node_neighbors if n in local]
-                    neighbor_union = local.union(node_neighbors)
-                    jaccard = float(len(local_neighbors)) / len(neighbor_union)
-                    if jaccard > 0.2:
-                        local_validated.add(node)
-                        community_hash[node].append(index)
-                
-                # print(local_validated == local)
-                # print(local)
-                if local_validated not in communities:
-                    communities.append(local_validated)
+                # print(new_neighbors)
+                if new_neighbors == neighbors:
+                    break
+                else:
+                    neighbors = new_neighbors
+                    for node in local:
+                        # final filter to check if the node is really belongs to the community
+                        node_neighbors = list(network.successors(node))  + list(network.predecessors(node))
+                        local_neighbors = [n for n in node_neighbors if n in local]
+                        neighbor_union = local.union(node_neighbors)
+                        jaccard = float(len(local_neighbors)) / len(neighbor_union)
+                        if jaccard > self.jaccard_threshold:
+                            local_validated.add(node)
+                            community_hash[node].add(index)
+                    
+                    if local_validated not in communities:
+                        communities.append(local_validated)
             index += 1
-    return (communities, community_hash)
+        return (communities, community_hash)
+
+
+    
 if __name__ == "__main__":
     ns = NetworkStats()
-    ns.init_from_file("../benchmark.csv")
 
-    bi_edges = find_triangles(ns.network)
-    communities, community_hash = find_community(ns.network, bi_edges)
-    # print(123)
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    ns.init_fome_directory(os.path.join(dir_path, "data/benchmark_1"))
+
+    detector = JYCommunityDetection()
+    communities, community_hash = detector.detect(ns.network)
+    
     print(communities)
     print(community_hash)
-
     print(list(nx.k_clique_communities(ns.network.to_undirected(), 4)))
-    # degrees = ns.degree_distribution()
-    # print("Degree distribution: {0}".format(degrees))
-
-    # # print(nx.triangles(ns.network))
-
-    # print(ns.network.has_edge(11, 14))
-    # print(ns.network.has_edge(14, 11))
--- network_stats.py
+++ network_stats.py
@@ -1,5 +1,5 @@
 import networkx as nx
-import sys
+import sys, os
 import re
 from collections import defaultdict
 import matplotlib.pyplot as plt
@@ -8,7 +8,7 @@ class NetworkStats(object):
 
     def __init__(self):
         self.network = nx.DiGraph()
-
+        self.communities = []
     # Extract edges data from snap network file
     def _get_edges(self, file_name):
         edges = []
@@ -20,61 +20,79 @@ class NetworkStats(object):
                     nodes = map(lambda s: int(s.replace("\n", " ").strip()), re.split("[,]", line))
                     edges.append(tuple(nodes))
         return edges
-
-    def init_from_file(self, file_name):
+    
+    def read_graph_from_file(self, file_name):
         edges = self._get_edges(file_name)
         self.network.add_edges_from(edges)
-        
-    def degree_distribution(self):
-        degrees = nx.degree(self.network)
-        degree_dist = defaultdict(int)
-        
-        for _, d in degrees.items():
-            degree_dist[d] += 1
-            
-        return degree_dist
     
-    def avg_degree(self):
-        degree_sum = sum(self.network.degree().values())
-        avg_degree = degree_sum / float(len(self.network.nodes()))
-        return avg_degree
 
-    def avg_coefficient(self):
-        return nx.average_clustering(self.network)
-
-    def diameter_of_largest_component(self):
-        sub_component = max(nx.connected_component_subgraphs(self.network), key=len)
-        return nx.diameter(sub_component)
-
-    def components(self):
-        return list(nx.connected_components(self.network))
-
-
-    def plot_distribution(self, distribution):
-        max_dist = 0
-        for dist, count in distribution.items():
-            max_dist = max(max_dist, dist)
+    # read the community information from file
+    def read_community_from_file(self, file_name):
+        memberships = defaultdict(set)
+        with open(file_name) as f:
+            for line in f:
+                node_s, communities = map(lambda s: s.replace("\n", " ").strip(), re.split("[\t]", line))
+                for commu in map(int, communities.split(" ")):
+                    memberships[commu].add(int(node_s))
         
-        vals = [0] * (max_dist + 1)
-        bins = range(max_dist + 2)
+        for _, nodes in memberships.items():
+            self.communities.append(nodes)
         
-        for dist, count in distribution.items():
-            vals[dist] = count
         
-        plt.hist(vals, bins=bins)
-        plt.show()
+    def init_fome_directory(self, directory):
+        self.read_graph_from_file(os.path.join(directory, "dataset.csv"))
+        self.read_community_from_file(os.path.join(directory, "community.txt"))
 
-if __name__ == "__main__":
-    file_name = sys.argv[1]
-    ns = NetworkStats()
-    ns.init_from_file(file_name)
+    # def degree_distribution(self):
+    #     degrees = nx.degree(self.network)
+    #     degree_dist = defaultdict(int)
+        
+    #     for _, d in degrees.items():
+    #         degree_dist[d] += 1
+            
+    #     return degree_dist
+    
+    # def avg_degree(self):
+    #     degree_sum = sum(self.network.degree().values())
+    #     avg_degree = degree_sum / float(len(self.network.nodes()))
+    #     return avg_degree
 
-    degrees = ns.degree_distribution()
-    print("Degree distribution: {0}".format(degrees))
-    ns.plot_distribution(degrees)
+    # def avg_coefficient(self):
+    #     return nx.average_clustering(self.network)
 
-    print("Average coefficient: {0}".format(ns.avg_coefficient()) )
-    print("Average degree: {0}".format(ns.avg_degree()) )
-    components = ns.components()
-    print("Number of components: {0}".format( len(components) ))
-    print("Diameter of largest component: {0}".format(ns.diameter_of_largest_component()))
+    # def diameter_of_largest_component(self):
+    #     sub_component = max(nx.connected_component_subgraphs(self.network), key=len)
+    #     return nx.diameter(sub_component)
+
+    # def components(self):
+    #     return list(nx.connected_components(self.network))
+
+
+    # def plot_distribution(self, distribution):
+    #     max_dist = 0
+    #     for dist, count in distribution.items():
+    #         max_dist = max(max_dist, dist)
+        
+    #     vals = [0] * (max_dist + 1)
+    #     bins = range(max_dist + 2)
+        
+    #     for dist, count in distribution.items():
+    #         vals[dist] = count
+        
+    #     plt.hist(vals, bins=bins)
+    #     plt.show()
+
+# if __name__ == "__main__":
+#     file_name = sys.argv[1]
+#     ns = NetworkStats()
+#     ns.init_from_file(file_name)
+
+#     degrees = ns.degree_distribution()
+#     print("Degree distribution: {0}".format(degrees))
+#     ns.plot_distribution(degrees)
+
+#     print("Average coefficient: {0}".format(ns.avg_coefficient()) )
+#     print("Average degree: {0}".format(ns.avg_degree()) )
+#     components = ns.components()
+#     print("Number of components: {0}".format( len(components) ))
+#     print("Diameter of largest component: {0}".format(ns.diameter_of_largest_component()))
